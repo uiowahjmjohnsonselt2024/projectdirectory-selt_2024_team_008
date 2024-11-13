38922eba46417665d260011d41b7db69d7fdc398
CavRiley <cavan-riley@uiowa.edu>
2024-11-12 18:42:16 -0600

    STYLE: rubocop style fixes

--- shards-of-the-grid/app/helpers/application_helper.rb
+++ shards-of-the-grid/app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
+# ApplicationHelper provides utility methods for views across the application.
+# These methods are accessible in views and can be used to reduce duplication.
 module ApplicationHelper
 end
