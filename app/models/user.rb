d982436498d3b18169ba59cbc327d3da2c2de1dc
Jacob Arkin <jarkin1@hotmail.com>
2024-11-14 12:40:58 -0600

    Added username option and method for either username or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
+
+  # Virtual attribute for authenticating by either username or email
+  attr_accessor :login
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+
+  # Override Devise's find_for_database_authentication method
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if (login = conditions.delete(:login))
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { value: login.downcase }]).first
+    else
+      where(conditions.to_h).first
+    end
+  end
 end
