ca6299c633715fe1d224e1326588a9ccf97e3a87
Jacob Arkin <jarkin1@hotmail.com>
2024-11-14 12:42:49 -0600

    Using app password now - normal password does not work

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,7 +49,7 @@ Rails.application.configure do
     port:                 587,
     domain:               'gmail.com',
     user_name:            ENV['GMAIL_USERNAME'],
-    password:             ENV['GMAIL_PASSWORD'],
+    password:             ENV['GMAIL_APP_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true
   }
