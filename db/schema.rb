3ec5031dca84490247fa50dbb1d0b0a34d7ac499
CavRiley <cavan-riley@uiowa.edu>
2024-11-16 18:08:22 -0600

    ADD: added db schema for shop items

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,17 @@
 ActiveRecord::Schema[7.0].define(version: 2024_11_14_182403) do
 # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  
+end
+
+ActiveRecord::Schema[7.0].define(version: 2024_11_16_232456) do
+  create_table "shop_items", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "price_in_shards"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
