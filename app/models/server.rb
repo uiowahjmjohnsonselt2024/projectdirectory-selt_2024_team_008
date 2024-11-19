5dbce99de613fff843399b240410ac5be87d07ac
Jacob Arkin <jarkin1@hotmail.com>
2024-11-19 17:15:51 -0600

    STYLE: Added more debug statements

--- app/models/server.rb
+++ app/models/server.rb
@@ -2,24 +2,35 @@
 
 class Server < ApplicationRecord
   belongs_to :creator, class_name: 'User'
-  has_many :memberships
+  has_many :memberships, dependent: :destroy
   has_many :users, through: :memberships
-  has_many :messages
+  has_many :messages, dependent: :destroy
 
   after_create :add_creator_to_memberships
 
   def user_can_access?(user)
-    users.exists?(user.id)
+    has_access = users.exists?(user.id)
+    Rails.logger.info("Checking access for user #{user.id} on server #{id}: #{has_access}")
+    has_access
   end
 
   def remove_user(user)
-    memberships.find_by(user: user)&.destroy
+    membership = memberships.find_by(user: user)
+    if membership
+      Rails.logger.info("Removing user #{user.id} from server #{id}")
+      membership.destroy
+    else
+      Rails.logger.warn("Attempted to remove user #{user.id} from server #{id}, but no membership found")
+    end
   end
 
   private
 
   def add_creator_to_memberships
     memberships.create(user: creator)
+    Rails.logger.info("Added creator #{creator.id} to memberships for server #{id}")
+  rescue StandardError => e
+    Rails.logger.error("Failed to add creator to memberships for server #{id}: #{e.message}")
   end
 
 end
