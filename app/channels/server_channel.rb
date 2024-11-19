a4779b9f42ebe54664f0043164cd9bfd883fc72d
Jacob Arkin <jarkin1@hotmail.com>
2024-11-18 23:37:50 -0600

    ADD: Added basic functionality for users joining/leaving servers

--- app/channels/server_channel.rb
+++ app/channels/server_channel.rb
@@ -1,9 +1,15 @@
 class ServerChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream_from "server_#{params[:server_id]}_channel"
+    ActionCable.server.broadcast "server_#{params[:server_id]}", message: "#{current_user.username} has joined the server"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+    stop_all_streams
+    ActionCable.server.broadcast "server_#{params[:server_id]}", message: "#{current_user.username} has left the server"
+
+    # Remove user from list of active users in the server
+    Server.remove_user(params[:server_id], current_user.id)
   end
 end
