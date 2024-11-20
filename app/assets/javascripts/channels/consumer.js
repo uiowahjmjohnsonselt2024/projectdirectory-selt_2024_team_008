e4a806d3b9f457cd874167fd8bfa070ed05e1e4e
Jacob Arkin <jarkin1@hotmail.com>
2024-11-19 18:50:22 -0600

    STYLE: Cleaned up

--- app/assets/javascripts/channels/consumer.js
+++ app/assets/javascripts/channels/consumer.js
@@ -5,8 +5,3 @@ const createConsumer = ActionCable.createConsumer;
 window.App || (window.App = {});
 App.cable = createConsumer();
 console.log("App.cable initialized:", App.cable);
-
-// import { createConsumer } from "@rails/actioncable";
-//
-// export default createConsumer();
-
