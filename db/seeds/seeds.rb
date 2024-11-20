a4e2cc7529c6c8d4b2927259fb56bb760ff39df4
Jacob Arkin <jarkin1@hotmail.com>
2024-11-19 19:55:20 -0600

    ADD: Updated seeds

--- db/seeds/seeds.rb
+++ db/seeds/seeds.rb
@@ -1,23 +1,25 @@
 # frozen_string_literal: true
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-# NOTE: Will probably want to remove this later, using it for dev
+# Ensure tables exist before seeding
+if ActiveRecord::Base.connection.table_exists?('users') &&
+  ActiveRecord::Base.connection.table_exists?('servers') &&
+  ActiveRecord::Base.connection.table_exists?('memberships')
 
-test_user = User.find_or_create_by!(email: 'test_user@example.com') do |user|
-  user.username = 'testuser'
-  user.password = 'password123' # Ensure this matches your application's requirements
+  puts "Running seeds in #{Rails.env} environment"
+
+  # Seed data shared between development and test environments
+  test_user = User.find_or_create_by!(email: 'test_user@example.com') do |user|
+    user.username = 'testuser'
+    user.password = 'password' # Ensure this matches your application's requirements
+  end
+
+  test_server = Server.find_or_create_by!(name: 'Test Server', creator: test_user)
+
+  Membership.find_or_create_by!(user: test_user, server: test_server)
+
+  puts "Seeded test_user with membership to Test Server in #{Rails.env} environment."
+else
+  puts "Skipping seeds as required tables do not exist."
 end
-
-test_server = Server.find_or_create_by!(name: 'Test Server', creator: test_user)
-
-Membership.find_or_create_by!(user: test_user, server: test_server)
-
-puts "Seeded test_user with membership to Test Server."
