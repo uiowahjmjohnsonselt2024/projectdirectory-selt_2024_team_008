b18344283c8a74de52076abb3e1ed5c218b3154e
Jacob Arkin <jarkin1@hotmail.com>
2024-11-19 19:54:51 -0600

    ADD: Updated

--- config/initializers/test_user_membership.rb
+++ config/initializers/test_user_membership.rb
@@ -1,18 +1,11 @@
-# config/initializers/test_user_membership.rb
-Rails.logger.info("test_user_membership.rb initializer loaded")
-Rails.application.config.to_prepare do
-  Rails.logger.info("Initializing test_user_membership.rb...")
+if ActiveRecord::Base.connection.table_exists?('users') && ActiveRecord::Base.connection.table_exists?('servers')
+  Rails.application.config.to_prepare do
+    test_user = User.find_by(email: 'test_user@example.com')
+    test_server = Server.find_by(name: 'Test Server')
 
-  test_user = User.find_by(email: 'test_user@example.com')
-  test_server = Server.find_by(name: 'Test Server')
-
-  if test_user && test_server
-    Membership.find_or_create_by!(user: test_user, server: test_server)
-    Rails.logger.info("Ensured test_user has membership to Test Server")
-  else
-    missing = []
-    missing << "test_user" if test_user.nil?
-    missing << "test_server" if test_server.nil?
-    Rails.logger.warn("Could not ensure test_user membership: Missing #{missing.join(' and ')}")
+    if test_user && test_server
+      Membership.find_or_create_by!(user: test_user, server: test_server)
+      Rails.logger.info("Ensured test_user has membership to Test Server")
+    end
   end
 end
