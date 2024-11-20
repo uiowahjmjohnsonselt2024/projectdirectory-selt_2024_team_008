1255c6a35f847b999e7aa76e6896780483776f34
Jacob Arkin <jarkin1@hotmail.com>
2024-11-19 18:52:21 -0600

    FIX: Messages properly formatted in Hash - Different way to check if a given user has access to a chat room server

--- app/channels/server_channel.rb
+++ app/channels/server_channel.rb
@@ -2,29 +2,33 @@ class ServerChannel < ApplicationCable::Channel
   def subscribed
     server = Server.find_by(id: params[:server_id])
 
-    if server && current_user && server.user_can_access?(current_user)
-      Rails.logger.info("Subscribed to server_#{params[:server_id]}")
-      stream_from "server_#{params[:server_id]}"
-      ActionCable.server.broadcast(
-        "server_#{params[:server_id]}",
-        { message: "#{current_user.username} has joined the server" }
-      )
+    if server && current_user
+      server.memberships.find_or_create_by!(user: current_user)
+      Rails.logger.info("Ensured user #{current_user.id} has membership to server #{server.id}")
+
+      if server.user_can_access?(current_user)
+        Rails.logger.info("Subscribed to server_#{params[:server_id]}")
+        stream_from "server_#{params[:server_id]}"
+        ActionCable.server.broadcast(
+          "server_#{params[:server_id]}",
+          { message: "#{current_user.username} has joined the chat room" }
+        )
+      else
+        Rails.logger.info("Subscription rejected for server_#{params[:server_id]}")
+        reject
+        end
     else
-      Rails.logger.info("Subscription rejected for server_#{params[:server_id]}")
       reject
-      end
     end
+  end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
     stop_all_streams
     ActionCable.server.broadcast(
       "server_#{params[:server_id]}",
-      { message: "#{current_user.username} has joined the server" }
+      { message: "#{current_user.username} has left the chat room" }
     )
-
-    # Remove user from list of active users in the server
-    server = Server.find_by(id: params[:server_id])
-    server&.remove_user(current_user)
+    Rails.logger.info("Stopped streaming for user #{current_user.id} on server #{params[:server_id]}")
   end
 end
