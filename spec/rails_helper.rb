b26a62012ca52a8af1fe7ff91ff371f4ab9fd085
Jacob Arkin <jarkin1@hotmail.com>
2024-11-20 00:45:57 -0600

    ADD: More database cleaner config added

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -40,6 +40,14 @@ RSpec.configure do |config|
     DatabaseCleaner.clean_with(:truncation)
   end
 
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
   config.around(:each) do |example|
     DatabaseCleaner.cleaning do
       example.run
