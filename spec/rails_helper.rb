d95665fbf4f887d5e2a4411580f50c8a99fdd9f3
Jacob Arkin <jarkin1@hotmail.com>
2024-11-19 22:46:42 -0600

    ADD: Added config for testing with Devise

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,6 +33,8 @@ end
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 
+  config.include Devise::Test::IntegrationHelpers, type: :request
+
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
