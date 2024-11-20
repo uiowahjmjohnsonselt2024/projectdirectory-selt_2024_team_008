a3785ddea186d9b33f8073ad223413a2b0a2d0d6
Jacob Arkin <jarkin1@hotmail.com>
2024-11-20 00:47:06 -0600

    ADD: Updates to tests

--- spec/requests/servers_spec.rb
+++ spec/requests/servers_spec.rb
@@ -6,13 +6,15 @@ RSpec.describe ServersController, type: :request do
   let(:valid_params) { { server: { name: "Test Server" } } }
   let(:invalid_params) { { server: { name: "" } } }
 
-  before do
+  before(:each) do
     sign_in user
+    Membership.destroy_all
+    Server.destroy_all
   end
 
   describe "GET /servers" do
     it "lists all servers the user has joined" do
-      Membership.find_or_create_by(user: user, server: server)
+      Membership.find_or_create_by!(user: user, server: server)
       get servers_path
       expect(response).to have_http_status(:ok)
     end
@@ -43,26 +45,23 @@ RSpec.describe ServersController, type: :request do
   describe "GET /servers/:id" do
     context "when the user is a member" do
       before do
-        Membership.find_or_create_by(user: user, server: server)
+        Membership.find_or_create_by!(user: user, server: server)
       end
 
       it "shows the server details" do
         get server_path(server)
         expect(response).to have_http_status(:ok)
-        expect(assigns(:server)).to eq(server)
-        expect(assigns(:messages)).to eq(server.messages)
+        expect(response.body).to include(server.name)
       end
     end
 
     context "when the user is not a member" do
       it "adds the user to the server's membership and logs the action" do
-        allow(Rails.logger).to receive(:info) 
+        allow(Rails.logger).to receive(:info)
 
-        # Create a server with a different creator
         another_user = create(:user)
         server = create(:server, creator: another_user)
 
-        # Ensure the test user is not already a member
         expect(server.memberships.exists?(user_id: user.id)).to be_falsey
 
         expect {
@@ -70,27 +69,30 @@ RSpec.describe ServersController, type: :request do
         }.to change(Membership, :count).by(1)
 
         expect(response).to have_http_status(:ok)
-
         expect(Rails.logger).to have_received(:info).with("Added user #{user.id} to server #{server.id}").at_least(:once)
       end
-
     end
 
     context "when an error occurs while adding the user to the server" do
       before do
-        allow_any_instance_of(Server).to receive(:memberships).and_raise(StandardError, "Membership creation failed")
+        # Allow any error to be logged
         allow(Rails.logger).to receive(:error)
+
+        # Stub `create!` to raise the error
+        allow_any_instance_of(ActiveRecord::Associations::CollectionProxy)
+          .to receive(:create!).and_raise(StandardError, "Membership creation failed")
       end
 
       it "logs the error and still proceeds to show the server" do
-        allow_any_instance_of(Server).to receive(:memberships).and_raise(StandardError, "Membership creation failed")
-        allow(Rails.logger).to receive(:error)
-
         get server_path(server)
 
+        # Ensure the response is still OK
         expect(response).to have_http_status(:ok)
+
+        # Verify the error message was logged
         expect(Rails.logger).to have_received(:error).with(/Membership creation failed/).at_least(:once)
       end
     end
   end
+
 end
