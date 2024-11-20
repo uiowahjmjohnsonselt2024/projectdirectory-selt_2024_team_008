0b7480ec1fad1f4dd0b87eae4b49c195fa76d4c8
Jacob Arkin <jarkin1@hotmail.com>
2024-11-19 22:48:17 -0600

    ADD: Memberships added post build

--- spec/factories/memberships.rb
+++ spec/factories/memberships.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory :membership do
     association :user
     association :server
+
+    after(:build) do |membership|
+      Membership.find_or_create_by!(user: membership.user, server: membership.server)
+    end
   end
 end
