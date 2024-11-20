ba7000633d29633f0d40161c4a29ed8c93b6961d
Jacob Arkin <jarkin1@hotmail.com>
2024-11-19 18:53:30 -0600

    FIX: New method for checking membership to chat room server

--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -1,4 +1,6 @@
 class ServersController < ApplicationController
+
+  before_action :ensure_membership, only: :show
   def index
     @servers = current_user.joined_servers
   end
@@ -13,7 +15,7 @@ class ServersController < ApplicationController
   end
 
   def show
-    @server = Server.find(params[:id])
+    @server = Server.includes(:memberships, :messages).find(params[:id])
     @messages = @server.messages
   end
 
@@ -21,6 +23,17 @@ class ServersController < ApplicationController
 
   def server_params
     params.require(:server).permit(:name)
+  end
+
+  def ensure_membership
+    @server = Server.find(params[:id])
+    return if @server.user_can_access?(current_user)
+
+    # Add the current user to the server's membership if they don't already have access
+    @server.memberships.create(user: current_user)
+    Rails.logger.info("Added user #{current_user.id} to server #{params[:id]}")
+  rescue StandardError => e
+    Rails.logger.error("Failed to add user #{current_user.id} to server #{params[:id]}: #{e.message}")
   end
 
 end
