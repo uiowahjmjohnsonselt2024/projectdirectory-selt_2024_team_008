9a402e0d821d39c0e49b3f3c9b7540782d7054be
Jacob Arkin <jarkin1@hotmail.com>
2024-11-21 03:02:03 -0600

    ADD: Add last seen at for users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_20_050549) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_20_223258) do
   create_table "memberships", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "server_id", null: false
@@ -53,6 +53,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_20_050549) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.datetime "last_seen_at"
     t.index "LOWER(username)", name: "index_users_on_lower_username", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
