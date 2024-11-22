b9455e52d7b4367faa5c0cc1043b65707a0dbb13
Jacob Arkin <jarkin1@hotmail.com>
2024-11-22 15:33:38 -0600

    WIP: Added new css/js files for precompiling

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( multiplayer_sessions.css multiplayer_sessions.js server.css server_channel.js consumer.js )
