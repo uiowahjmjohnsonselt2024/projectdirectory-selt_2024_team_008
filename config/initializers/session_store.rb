71a0124601afe0967191d3c46f271c6a7a2d25e9
Jacob Arkin <jarkin1@hotmail.com>
2024-11-23 03:11:08 -0600

    configure redis

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,18 +2,14 @@
 
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_shards-of-the-grid_session'
 
 Rails.application.config.session_store :redis_store, {
   servers: [
     {
-      host: ENV['REDIS_HOST'],
-      port: ENV['REDIS_PORT'],
-      password: ENV['REDIS_PASSWORD'],
-      db: 0,
+      url: ENV['REDIS_URL'], # Use the full Redis URL from the environment variable
       namespace: 'session'
     }
   ],
   expire_after: 90.minutes,
-  key: '_your_app_session'
+  key: '_shards_of_the_grid_session'
 }
