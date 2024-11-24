3be69fd2d153126912b3e20369eac0107a89e482
Jacob Arkin <jarkin1@hotmail.com>
2024-11-23 21:11:05 -0600

    ADD: Migration for user roles

--- config/initializers/redis.rb
+++
@@ -1,9 +0,0 @@
-require 'redis'
-
-Redis.current = Redis.new(
-  host: ENV.fetch('REDIS_HOST', '127.0.0.1'),
-  port: ENV.fetch('REDIS_PORT', 6379),
-  password: ENV.fetch('REDIS_PASSWORD', nil),
-  url: ENV.fetch('REDIS_URL', 'redis://127.0.0.1:6379/0'),
-  ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
-)
