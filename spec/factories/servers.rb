d0e19f3fdda8669614be45b27b31ea72749c3439
Jacob Arkin <jarkin1@hotmail.com>
2024-11-27 17:18:26 -0600

    WIP: Factory adjustment

--- spec/factories/servers.rb
+++ spec/factories/servers.rb
@@ -4,6 +4,14 @@ FactoryBot.define do
     association :creator, factory: :user
     game { nil } # Allow the server to be created without a game initially
 
+    after(:create) do |server, evaluator|
+      server.update!(
+        original_creator_id: server.creator.id,
+        original_creator_name: server.creator.username,
+        original_creator_email: server.creator.email
+      )
+    end
+
     after(:build) do |server|
       if server.game.present? && server.name.blank?
         server.name = "Chat Room for #{server.game.name}"
