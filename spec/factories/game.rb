481a4dff67f0d6e923a946318c73abcd66fa7b40
Jacob Arkin <jarkin1@hotmail.com>
2024-11-26 22:38:27 -0600

    WIP: Updates to factory

--- spec/factories/game.rb
+++ spec/factories/game.rb
@@ -1,6 +1,12 @@
 FactoryBot.define do
   factory :game do
-    sequence(:name) { |n| "Game #{n}" }
-    game_status { "active" }
+    name { "Test Game" }
+    creator { association(:user) }
+    game_status { "waiting" }
+
+    after(:build) do |game|
+      # Build the server associated with the game
+      game.server = build(:server, creator: game.creator, game: game) unless game.server
+    end
   end
 end
--- spec/factories/servers.rb
+++ spec/factories/servers.rb
@@ -2,5 +2,12 @@ FactoryBot.define do
   factory :server do
     sequence(:name) { |n| "Server #{n}" }
     association :creator, factory: :user
+    game { nil } # Allow the server to be created without a game initially
+
+    after(:build) do |server|
+      if server.game.present? && server.name.blank?
+        server.name = "Chat Room for #{server.game.name}"
+      end
+    end
   end
 end
