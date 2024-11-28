2bdd325876dad065d719be48032998055b40cd6b
Jacob Arkin <jarkin1@hotmail.com>
2024-11-28 16:08:39 -0600

    WIP: Updates - Remove game_status from games table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_28_093218) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_28_103658) do
   create_table "games", force: :cascade do |t|
     t.string "name", null: false
     t.integer "creator_id", null: false
