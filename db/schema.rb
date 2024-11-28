a1d1a5f46077aea4762e0f32025e174d4f239a69
Jacob Arkin <jarkin1@hotmail.com>
2024-11-28 03:52:34 -0600

    ADD: Column for grid

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_27_122740) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_28_093218) do
   create_table "games", force: :cascade do |t|
     t.string "name", null: false
     t.integer "creator_id", null: false
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_27_122740) do
     t.datetime "ended_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "grid"
   end
 
   create_table "items", force: :cascade do |t|
