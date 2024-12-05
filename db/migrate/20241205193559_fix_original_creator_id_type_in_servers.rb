562425e59429764e9f837cc49e95bfb4c203da0d
Jacob Arkin <jarkin1@hotmail.com>
2024-12-05 16:27:26 -0600

    FIX: Updated file to hopefully work in production

--- db/migrate/20241205193559_fix_original_creator_id_type_in_servers.rb
+++ db/migrate/20241205193559_fix_original_creator_id_type_in_servers.rb
@@ -1,5 +1,9 @@
 class FixOriginalCreatorIdTypeInServers < ActiveRecord::Migration[7.0]
   def change
-    change_column :servers, :original_creator_id, :integer, null: true
+    if ActiveRecord::Base.connection.adapter_name == 'PostgreSQL'
+      change_column :servers, :original_creator_id, :integer, using: 'original_creator_id::integer', null: true
+    else
+      change_column :servers, :original_creator_id, :integer, null: true
+    end
   end
 end
