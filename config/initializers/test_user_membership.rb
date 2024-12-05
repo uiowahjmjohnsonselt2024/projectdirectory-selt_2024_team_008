40560bff7c6a7d05e548eb17bdfa460d777f10de
Jacob Arkin <jarkin1@hotmail.com>
2024-12-05 17:06:50 -0600

    Wrap in rails group == assets

--- config/initializers/test_user_membership.rb
+++ config/initializers/test_user_membership.rb
@@ -1,11 +1,13 @@
-if ActiveRecord::Base.connection.table_exists?('users') && ActiveRecord::Base.connection.table_exists?('servers')
-  Rails.application.config.to_prepare do
-    test_user = User.find_by(email: 'test_user@example.com')
-    test_server = Server.find_by(name: 'Test Server')
+unless ENV['RAILS_GROUPS'] == 'assets'
+  if ActiveRecord::Base.connection.table_exists?('users') && ActiveRecord::Base.connection.table_exists?('servers')
+    Rails.application.config.to_prepare do
+      test_user = User.find_by(email: 'test_user@example.com')
+      test_server = Server.find_by(name: 'Test Server')
 
-    if test_user && test_server
-      Membership.find_or_create_by!(user: test_user, server: test_server)
-      Rails.logger.info("Ensured test_user has membership to Test Server")
+      if test_user && test_server
+        Membership.find_or_create_by!(user: test_user, server: test_server)
+        Rails.logger.info("Ensured test_user has membership to Test Server")
+      end
     end
   end
 end
