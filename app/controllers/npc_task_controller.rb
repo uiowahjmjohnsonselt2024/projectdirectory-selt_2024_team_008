5ded167c906e430f345acb456ad7f0859e4d78d7
ebayer <evin-bayer@uiowa.edu>
2024-12-07 14:09:31 -0600

    WIP added method for updating shard balance when user guesses the correct answer

--- app/controllers/npc_task_controller.rb
+++ app/controllers/npc_task_controller.rb
@@ -1,5 +1,8 @@
 class NpcTaskController < ApplicationController
   def chat
+    Rails.logger.info("Current User: #{current_user.inspect}")
+    Rails.logger.info("ShardAccount: #{current_user.shard_account.inspect}")
+
     user_message = params[:message]
     if user_message.blank?
       render json: { error: "Message parameter is required" }, status: :bad_request
@@ -17,8 +20,15 @@ class NpcTaskController < ApplicationController
         max_tokens: 50
       }
     )
-    Rails.logger.info("OpenAI API response: #{response}")
+    #Rails.logger.info("OpenAI API response: #{response}")
     npc_message = response.dig('choices', 0, 'message', 'content')
+
+    if npc_message&.downcase == "correct!"
+      shard_account = current_user.shard_account
+      shard_account.balance += 50
+      shard_account.save!
+    end
+
     render json: { npc_message: npc_message }
   rescue StandardError => e
     render json: { error: "Something went wrong: #{e.message}" }, status: :internal_server_error
