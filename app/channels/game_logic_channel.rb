27273267d7aa12cf57464af78669bf5fe9f0a907
Jacob Arkin <jarkin1@hotmail.com>
2024-12-06 19:58:26 -0600

    WIP: Add game logic channel

--- app/channels/game_logic_channel.rb
+++ app/channels/game_logic_channel.rb
@@ -1,9 +1,62 @@
 class GameLogicChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    game = Game.find_by(id: params[:game_id])
+
+    if game && current_user
+      stop_all_streams
+      stream_for game
+
+      # Notify all players that a user has joined
+      # broadcast_system_message("#{current_user.username} has joined the game.", game)
+
+      # Send the current game state to the new subscriber
+      # transmit_game_state(game)
+    else
+      reject
+    end
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+    stop_all_streams
+
+    game = Game.find_by(id: params[:game_id])
+    return unless game && current_user
+
+    # Notify all players that a user has left
+    # broadcast_system_message("#{current_user.username} has left the game.", game)
+  end
+
+  def make_move(data)
+    game = Game.find_by(id: params[:game_id])
+    return unless game
+
+    x = data['x'].to_i
+    y = data['y'].to_i
+
+    # Validate and process the move
+    if valid_move?(game, x, y)
+      # Update game state
+      game.grid[y][x] = current_user.id
+      game.save!
+
+      # Broadcast updated game state
+      GameLogicChannel.broadcast_to(game, { type: 'game_state', grid: game.grid, user_id: current_user.id, x: x, y: y })
+    else
+      transmit(error: "Invalid move")
+    end
+  end
+
+  private
+
+  def valid_move?(game, x, y)
+    x.between?(0, 5) && y.between?(0, 5) && game.grid[y][x].nil?
+  end
+
+  def transmit_game_state(game)
+    transmit({ type: 'game_state', grid: game.grid })
+  end
+
+  def broadcast_system_message(message, game)
+    GameLogicChannel.broadcast_to(game, { type: 'system', message: message })
   end
 end
