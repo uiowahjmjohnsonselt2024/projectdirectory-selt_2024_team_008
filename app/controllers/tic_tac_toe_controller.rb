20714c7f6ba93f062c5cb398f7c22deb6a1ef1b2
Matt Stensby <mattystenz@gmail.com>
2024-12-10 16:29:15 -0600

    WIP: Beginning the Tic Tac Toe Basic setup. Going to move to setting up HTML and CSS pages (as well as switching the route for testing)

---
+++ app/controllers/tic_tac_toe_controller.rb
@@ -0,0 +1,27 @@
+class TicTacToeController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_game
+  before_action :set_server
+  before_action :initialize_game_state, only: [:index]
+  respond_to :html, :json
+  def index
+
+  end
+
+  def set_game
+    @game = Game.find_by(id: params[:id])
+    unless @game
+      respond_to do |format|
+        format.html { redirect_to root_path, alert: "Game not found." }
+        format.json { render json: { error: "Game not found" }, status: :not_found }
+      end
+    end
+  end
+  def set_server
+    @server = @game.server
+  end
+  def initialize_game_state
+
+  end
+
+end
