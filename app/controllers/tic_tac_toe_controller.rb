0d09802a944217f4946e26a56d3b5b0c19c7267a
Matt Stensby <mattystenz@gmail.com>
2024-12-10 19:29:18 -0600

    WIP: Completed a portion of the end-game scenario where the user (or the CPU) gets one of the 8 possible combinations of a victory in Tic Tac Toe.

--- app/controllers/tic_tac_toe_controller.rb
+++ app/controllers/tic_tac_toe_controller.rb
@@ -1,28 +1,38 @@
 class TicTacToeController < ApplicationController
-  # before_action :authenticate_user!
-  # before_action :set_game
-  # before_action :set_server
-  # before_action :initialize_game_state, only: [:index]
-  # respond_to :html, :json
-  # def index
-  #
-  # end
-  #
-  #
-  # def set_game
-  #   @game = Game.find_by(id: params[:id])
-  #   unless @game
-  #     respond_to do |format|
-  #       format.html { redirect_to root_path, alert: "Game not found." }
-  #       format.json { render json: { error: "Game not found" }, status: :not_found }
-  #     end
-  #   end
-  # end
-  # def set_server
-  #   @server = @game.server
-  # end
-  # def initialize_game_state
-  #
-  # end
+  before_action :authenticate_user!
+
+  respond_to :html, :json
+  def index
+    @shard_balance = current_user.shard_account.balance
+  end
+
+
+
+
+  def process_game_logic(move, board, current turn)
+
+  end
+
+  #Checks if the board is filled. If so game will turn out to be a Scratch (as long as nobody won on the last move)
+  def board_full?(board)
+    board.all?
+  end
+
+  def winner?(board, player)
+
+    # List of all winning positions for either the user or the CPU
+    winning_posiitons = [
+      [0,1,2], [3,4,5], [6,7,8],
+      [0,3,6], [1,4,7], [2,5,8],
+      [0,4,8], [2,4,6]
+    ]
+
+
+    # Checks each winning position with the input of the user and CPU after every move.
+    winning_posiitons.each do |input|
+      return true if input.all? { |i| board[i] == player}
+    end
+    false
+  end
 
 end
