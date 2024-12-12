057873b30625d2601fba484c8a2a68d63474d8f1
Jacob Arkin <jarkin1@hotmail.com>
2024-12-12 16:01:26 -0600

    FIX: Updated tests to use new Tile model

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,11 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
+  let(:user) { User.create!(email: "test@example.com", username: "testuser", password: "password") }
+  let!(:game) do
+    server = Server.create!(name: "Chat Room for Test Game", creator_id: user.id)
+    Game.create!(name: "Test Game", creator_id: user.id, server_id: server.id).tap do |game|
+      server.update!(game_id: game.id)
+    end
+  end
+
   describe "#find_user_position" do
-    let(:game) { Game.new(grid: Array.new(6) { Array.new(6, nil) }) }
 
     before do
-      game.grid[2][3] = "testuser" # Place "testuser" at (3, 2)
+      tile = game.tiles.find_by(x: 3, y: 2) # Find tile at (3, 2)
+      tile.update!(occupant: "testuser")
     end
 
     it "returns the position of the user if present" do
@@ -18,10 +26,9 @@ RSpec.describe Game, type: :model do
   end
 
   describe "#tile_empty?" do
-    let(:game) { Game.new(grid: Array.new(6) { Array.new(6, nil) }) }
-
     before do
-      game.grid[1][1] = "testuser" # Place "testuser" at (1, 1)
+      tile = game.tiles.find_by(x: 1, y: 1) # Find tile at (1, 1)
+      tile.update!(occupant: "testuser")
     end
 
     it "returns true if the tile is empty" do
