f7d6b46c26cb12cefb83703b418c4ced4216af7f
Matt Stensby <mattystenz@gmail.com>
2024-12-12 20:46:16 -0600

    WIP: Adjusted the tic tac toe files to make the game work. Also implemented the tasks so that when it is clicked the user is able to play tic tac toe. Now moving onto being able to integrate the npc task as well as the math task.

--- config/routes.rb
+++ config/routes.rb
@@ -166,7 +166,4 @@ Rails.application.routes.draw do
   # Example resource route within a namespace:
   #   namespace :admin do
   #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-end
+  #     # (app/contr
