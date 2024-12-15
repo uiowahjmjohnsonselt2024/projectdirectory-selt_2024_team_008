3d9e7eec86865e808169c4201a0b84c5d511151f
ebayer <evin-bayer@uiowa.edu>
2024-12-14 18:00:23 -0600

    Adding tests for the mystery box page

--- spec/features/mystery_boxes_spec.rb
+++ spec/features/mystery_boxes_spec.rb
@@ -1,7 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe "MysteryBoxes", type: :request do
-  describe "GET /show" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe "MysteryBoxesController", type: :feature do
+  let!(:user) { User.create!(email: "test@example.com", password: "password", username: "testuser") }
+  let!(:mystery_box_item) { Item.create!(item_name: "Mystery Box", item_type: "box") }
+  let!(:random_item) { Item.create!(item_name: "Random Item", item_type: "reward", images: "random_item.png") }
+  let!(:shard_account) { ShardAccount.create!(user: user, balance: 50) }
+
+  before do
+    login_as user, scope: :user
+  end
+
+  describe "GET #open" do
+    it "renders the page and displays mystery box count" do
+
+      UserItem.create!(user: user, item: mystery_box_item, quantity: 3)
+
+      visit open_mystery_boxes_path
+      expect(page).to have_content("3 Mystery Boxes")
+    end
+
+    it "renders the page  with zero mystery boxes if user has none" do
+      visit open_mystery_boxes_path
+      expect(page).to have_content("0 Mystery Boxes")
+    end
   end
 end
